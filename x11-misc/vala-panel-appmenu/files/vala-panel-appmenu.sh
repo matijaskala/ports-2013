9d504c8d00f11e31e1c4b74a95cc4365bff65818
Matija Skala <mskala@gmx.com>
2020-03-24 11:47:20 +0100

    updates by Skala
    merged:
    
      gentoo: f9528fdc339b85968a16c2a5ff5bdb73b818bf4a
      party-overlay: c61b22e3215b7005d0507a9a271010728a5d929f
      ubuntu-overlay: d5532823f249a93b284113c69c9365ed36c0a6ea
      stable-overlay: 0819ea53d72d7451432a285001942ee720371d71
      elementary: 8b20be0bad920b04901bdd49a2bd7ef5becd3566
      funtoo-overlay: dd76fcaaebc70b5de863f6395239a53e9e9d8dae
      sabayon-for-gentoo: 522599ec1779dbb817c715a26d9993f1866c376b
      sabayon-tools: 471ff9eb037189aca0a97d664d96872d9a38dab9
      gamerlay: be9dcd19cc898481ef3ba28c0c2174c35735f826
      unity-gentoo: 125ab13e6ef7a8401d8a733adffde716597852cd
      gnome-next: aa83e3a6f79bab999cecb69b82967264f07e4477
      flatpak-overlay: ea16fa7c90c16c8720e4a388e7ddcdd70ad30221

--- x11-misc/vala-panel-appmenu/files/vala-panel-appmenu.sh
+++
@@ -1,9 +0,0 @@
-# Set the GTK_MODULES env variable to load appmenu-gtk-module
-
-if [ -z "$GTK_MODULES" ]; then
-    GTK_MODULES="appmenu-gtk-module"
-else
-    GTK_MODULES="$GTK_MODULES:appmenu-gtk-module"
-fi
-export GTK_MODULES
-export UBUNTU_MENUPROXY=1
